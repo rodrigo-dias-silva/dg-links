a46f0a95d8ed0d2b7e955afe273a021d5364cb89
rodrigo-dias-silva <rodrigo-cleiton@hotmail.com>
2022-11-08 08:43:30 -0300

    config rotas com react router dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Home from './pages/Home/index';
+import Login from './pages/Login/index';
+import Admin from './pages/Admin/index';
+import Error from "./pages/Error";
 
-export default App;
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/admin',
+    element: <Admin />
+  },
+  {
+    path: '*',
+    element: <Error />
+  }
+])
+
+export { router };
