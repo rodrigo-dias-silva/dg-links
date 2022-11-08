14b52c2d321285b3ddcd5020512d079cba48994f
rodrigo-dias-silva <rodrigo-cleiton@hotmail.com>
2022-11-08 08:39:39 -0300

    adiçao pasta para arquivos components

---
+++ src/components/Logo/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Logo() {
+  return (
+    <Link to={'/'}>
+      <h1 className='text-white text-6xl font-bold'>
+        Dg<span className='text-transparent bg-gradient-to-b from-cyan-500 to-emerald-200 bg-clip-text font-semibold'>Links</span>
+      </h1>
+    </Link>
+  )
+}
