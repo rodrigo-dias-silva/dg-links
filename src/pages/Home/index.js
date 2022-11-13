7c9f85c6d73da77a8af86595c3ac6c34fb260c03
rodrigo-dias-silva <rodrigo-cleiton@hotmail.com>
2022-11-12 22:20:07 -0300

    reestruturaçao da pagina, adiçao de funcionalidades e renderizaçao condicional de acordo com o database

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,31 +1,83 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import { getDocs, collection, orderBy, query } from 'firebase/firestore'
+
+import { db } from '../../services/firebaseConnection'
 
 import img from '../../assets/404.gif'
+import Header from './../../components/Header/index';
+import Logo from './../../components/Logo/index';
 
 export default function Home() {
-  return (
-    <div className='flex flex-col items-center justify-center min-h-screen py-4 gap-6'>
-      <img src={img} alt='foto da pessoa' className='w-40 h-40  rounded-full border-2 border-white bg-center shadow-md' />
-      <h1 className='text-white text-2xl'>@digle.silva</h1>
-      <span className='text-slate-400'>Meus links</span>
 
-      <main className='flex flex-col max-w-xl w-10/12 gap-5 text-center'>
-        <section className='flex justify-center items-center bg-white hover:bg-red-600 hover:text-white w-full rounded py-2 hover:scale-105 transition'>
-          <a href='/'>
-            <span className='text-lg'>Canal do Youtube</span>
-          </a>
-        </section>
-        <section className='bg-white w-full rounded py-2  hover:scale-105 transition hover:bg-black hover:text-white'>
-          <a href='/'>
-            <span className='text-lg'>Tik Tok</span>
-          </a>
-        </section>
-        <section className='bg-white w-full hover:bg-indigo-500 hover:text-white rounded py-2 hover:scale-105 transition'>
-          <a href='/'>
-            <span className='text-lg'>Canal do Discord</span>
-          </a>
-        </section>
-      </main>
+  const [links, setLinks] = useState([])
+
+  useEffect(() => {
+    function loadLinks() {
+      const linksRef = collection(db, 'links')
+      const queryRef = query(linksRef, orderBy('created', 'asc'))
+
+      getDocs(queryRef)
+        .then((snapshot) => {
+          let list = []
+
+          snapshot.forEach((doc) => {
+            list.push({
+              id: doc.id,
+              name: doc.data().name,
+              url: doc.data().url,
+              bg: doc.data().bg,
+              color: doc.data().color
+            })
+          })
+
+          setLinks(list)
+        })
+    }
+
+    loadLinks()
+
+  }, [])
+
+  return (
+    <div className='scrollbar-hide min-h-screen'>
+
+      <div className='w-full flex-1 flex justify-center items-center px-4'>
+        <Header />
+      </div>
+
+      <div className='mt-20 w-full min-h-[600px] md:h-3/4 flex flex-1 flex-col  items-center gap-5'>
+
+        <img src={img} alt='foto da pessoa' className='w-40 h-40  rounded-full border-2 border-white bg-center shadow-md' />
+
+        <h1 className='text-white text-2xl'>@digle.silva</h1>
+
+        <span className='text-slate-400 mt-3'>Meus links</span>
+
+        <main className='flex flex-col max-w-xl w-10/12 gap-5 text-center'>
+
+          {links.map((item) => (
+
+            <section
+              key={item.id}
+              style={{ backgroundColor: item.bg }}
+              className='flex justify-center items-center w-full rounded py-2 hover:scale-105 transition'>
+              <a href={item.url} target='blank' className='w-full'>
+                <span style={{ color: item.color }} className='text-lg'>{item.name}</span>
+              </a>
+            </section>
+
+          ))}
+
+        </main>
+      </div>
+
+      <footer className='mt-12'>
+        <div className='flex justify-center items-center'>
+          <Logo />
+        </div>
+      </footer>
+
     </div>
   )
 }
