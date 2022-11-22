483913458142be275c4b010f2dae3d33d1ac8a4d
rodrigo-dias-silva <rodrigo-cleiton@hotmail.com>
2022-11-22 16:40:13 -0300

    config storage no firebase

--- src/services/firebaseConnection.js
+++ src/services/firebaseConnection.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBDwm-QGTXTmbp31T2SRdU-SIB2HQ2ejyY",
@@ -15,5 +16,6 @@ const firebaseConfig = {
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
 const auth = getAuth(firebaseApp);
+const storage = getStorage(firebaseApp)
 
-export { db, auth };
+export { db, auth, storage };
