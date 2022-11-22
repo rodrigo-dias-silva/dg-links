a47672b3478e4aa551b1defeaed8e417ad244eda
rodrigo-dias-silva <rodrigo-cleiton@hotmail.com>
2022-11-22 16:43:17 -0300

    atualizaçao de css

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom'
 
 export default function Logo() {
   return (
-    <Link to={'/'} className='p-12'>
-      <h1 className='text-white text-6xl font-bold'>
+    <Link to={'/admin'} className='p-10'>
+      <h1 className='text-white text-4xl font-bold'>
         Dg<span className='text-transparent bg-gradient-to-b from-cyan-500 to-emerald-200 bg-clip-text font-semibold'>Links</span>
       </h1>
     </Link>
